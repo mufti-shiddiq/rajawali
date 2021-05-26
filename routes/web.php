64c6473376460aeb5c1b8ff488daac06a541ecbc
Mufti <muftishid@gmail.com>
2021-05-27 04:14:39 +0700

    fix: routes middleware

--- routes/web.php
+++ routes/web.php
@@ -33,58 +33,64 @@ Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
-Route::get('/home', [App\Http\Controllers\DashboardController::class, 'index'])->name('home')->middleware('auth');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/home', [App\Http\Controllers\DashboardController::class, 'index'])->name('home');
+});
 
-Route::resource("users", UserController::class)->middleware('can:isAdmin');
-Route::get('/users/{id}/change_password', [UserController::class, 'changepw'])->name('users.changepw')->middleware('can:isAdmin');
-Route::put('/users/{id}/update_password', [UserController::class, 'updatepw'])->name('users.updatepw')->middleware('can:isAdmin');
+Route::group(['middleware' => ['can:isAdmin']], function () {
+    Route::resource("users", UserController::class);
+    Route::get('/users/{id}/change_password', [UserController::class, 'changepw'])->name('users.changepw');
+    Route::put('/users/{id}/update_password', [UserController::class, 'updatepw'])->name('users.updatepw');
+});
 
-Route::resource('categories', CategoryController::class)->middleware('auth');
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('categories', CategoryController::class);
 
-Route::resource('units', UnitController::class)->middleware('auth');
+    Route::resource('units', UnitController::class);
 
-Route::resource('customers', CustomerController::class)->middleware('auth');
+    Route::resource('customers', CustomerController::class);
 
-Route::resource('suppliers', SupplierController::class)->middleware('auth');
+    Route::resource('suppliers', SupplierController::class);
 
-Route::resource('products', ProductController::class)->middleware('auth');
-// Route::get('/products/stock_in', [ProductController::class, 'stock_in.index'])->name('stock_in.index')->middleware('auth');
-// Route::get('/products/stock_in/create', [ProductController::class, 'stock_in.create'])->name('stock_in.create')->middleware('auth');
-// Route::post('/products/stock_in/store', [ProductController::class, 'stock_in.store'])->name('stock_in.store')->middleware('auth');
+    Route::resource('products', ProductController::class);
+    Route::get('product-import-export', [ProductController::class, 'importExport'])->name('product-import-export');
+    Route::post('product-import', [ProductController::class, 'import'])->name('product-import');
+    Route::get('product-export', [ProductController::class, 'export'])->name('product-export');
 
-Route::resource('stock_in', StockInController::class)->middleware('auth');
-Route::resource('stock_out', StockOutController::class)->middleware('auth');
+    Route::resource('stock_in', StockInController::class)->middleware('auth');
+    Route::resource('stock_out', StockOutController::class)->middleware('auth');
+});
 
-// Route::resource('transactions', TransactionController::class)->middleware('auth');
-Route::get('/transactions', [TransactionController::class, 'index'])->name('transaction.index')->middleware('auth');
-Route::post('/transactions/add', [TransactionController::class, 'add'])->name('cart.store');
-Route::post('/transactions/update', [TransactionController::class, 'update'])->name('cart.update');
-Route::delete('/transactions/remove', [TransactionController::class, 'remove'])->name('cart.remove');
-Route::post('/transactions/clear', [TransactionController::class, 'clear'])->name('cart.clear');
-Route::post('/transactions/process', [TransactionController::class, 'process'])->name('transaction.process');
-Route::get('/transactions/success', [TransactionController::class, 'success'])->name('transaction.success');
-Route::get('/transactions/print/{id}', [TransactionController::class, 'print'])->name('transaction.print');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/transactions', [TransactionController::class, 'index'])->name('transaction.index');
+    Route::post('/transactions/add', [TransactionController::class, 'add'])->name('cart.store');
+    Route::post('/transactions/update', [TransactionController::class, 'update'])->name('cart.update');
+    Route::delete('/transactions/remove', [TransactionController::class, 'remove'])->name('cart.remove');
+    Route::post('/transactions/clear', [TransactionController::class, 'clear'])->name('cart.clear');
+    Route::post('/transactions/process', [TransactionController::class, 'process'])->name('transaction.process');
+    Route::get('/transactions/success', [TransactionController::class, 'success'])->name('transaction.success');
+    Route::get('/transactions/print/{id}', [TransactionController::class, 'print'])->name('transaction.print');
+});
 
-Route::get('/reports/transaction', [ReportController::class, 'transaction'])->name('reports.transaction');
-Route::get('/reports/transaction/{id}', [ReportController::class, 'trx_detail'])->name('reports.trx_detail');
-Route::delete('/reports/transaction/{id}', [ReportController::class, 'trx_destroy'])->name('reports.trx_destroy');
-Route::get('/reports/stock_in', [ReportController::class, 'stock_in'])->name('reports.stock_in');
-Route::get('/reports/stock_out', [ReportController::class, 'stock_out'])->name('reports.stock_out');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/reports/transaction', [ReportController::class, 'transaction'])->name('reports.transaction');
+    Route::get('/reports/transaction/{id}', [ReportController::class, 'trx_detail'])->name('reports.trx_detail');
+    Route::delete('/reports/transaction/{id}', [ReportController::class, 'trx_destroy'])->name('reports.trx_destroy');
+    Route::get('/reports/stock_in', [ReportController::class, 'stock_in'])->name('reports.stock_in');
+    Route::get('/reports/stock_out', [ReportController::class, 'stock_out'])->name('reports.stock_out');
 
-Route::get('/reports/daily', [ReportController::class, 'daily'])->name('reports.daily');
-Route::get('/reports/daily2', [ReportController::class, 'daily2'])->name('reports.daily2');
-Route::get('/reports/weekly', [ReportController::class, 'weekly'])->name('reports.weekly');
-Route::get('/reports/monthly', [ReportController::class, 'monthly'])->name('reports.monthly');
+    Route::get('/reports/daily', [ReportController::class, 'daily'])->name('reports.daily');
+    Route::get('/reports/daily2', [ReportController::class, 'daily2'])->name('reports.daily2');
+    Route::get('/reports/weekly', [ReportController::class, 'weekly'])->name('reports.weekly');
+    Route::get('/reports/monthly', [ReportController::class, 'monthly'])->name('reports.monthly');
+});
 
-// Route::resource('/wallets', WalletController::class);
-Route::get('/wallets', [WalletController::class, 'index'])->name('wallets.index');
-Route::get('/wallets/add_cash_in', [WalletController::class, 'add_cash_in'])->name('wallets.add_cash_in');
-Route::get('/wallets/add_cash_out', [WalletController::class, 'add_cash_out'])->name('wallets.add_cash_out');
-Route::post('/wallets/store_cash_in', [WalletController::class, 'store_cash_in'])->name('wallets.store_cash_in');
-Route::post('/wallets/store_cash_out', [WalletController::class, 'store_cash_out'])->name('wallets.store_cash_out');
-Route::delete('/wallets/{id}', [WalletController::class, 'destroy'])->name('wallets.destroy');
-
-Route::get('product-import-export', [ProductController::class, 'importExport'])->name('product-import-export');
-Route::post('product-import', [ProductController::class, 'import'])->name('product-import');
-Route::get('product-export', [ProductController::class, 'export'])->name('product-export');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/wallets', [WalletController::class, 'index'])->name('wallets.index');
+    Route::get('/wallets/add_cash_in', [WalletController::class, 'add_cash_in'])->name('wallets.add_cash_in');
+    Route::get('/wallets/add_cash_out', [WalletController::class, 'add_cash_out'])->name('wallets.add_cash_out');
+    Route::post('/wallets/store_cash_in', [WalletController::class, 'store_cash_in'])->name('wallets.store_cash_in');
+    Route::post('/wallets/store_cash_out', [WalletController::class, 'store_cash_out'])->name('wallets.store_cash_out');
+    Route::delete('/wallets/{id}', [WalletController::class, 'destroy'])->name('wallets.destroy');
+});
