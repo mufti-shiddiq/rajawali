72244977c2477a97f1ed8417f277e5c452dca59e
Mufti <muftishid@gmail.com>
2021-05-27 01:31:51 +0700

    fix: products migration

---
+++ database/migrations/2021_05_27_013106_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string("code");
+            $table->string("product_name");
+            $table->integer("category_id");
+            $table->integer("unit_id");
+            $table->integer("stock");
+            $table->integer("sold");
+            $table->integer("buy_price");
+            $table->integer("sell_price");
+            $table->integer("created_by");
+            $table->integer("updated_by");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
