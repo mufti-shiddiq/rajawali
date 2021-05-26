d2e480acfa28442beda4513ab11ff8c860af683e
Mufti <muftishid@gmail.com>
2021-05-27 01:55:03 +0700

    change users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username', 255)->unique();
+            $table->string('username');
             $table->string("name");
             $table->string('password');
             $table->string("role");
